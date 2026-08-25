f1ff4217ded50bbc37290dcdd8658b70c61887c3
brunobvaz@gmail.com <bbv19008>
2026-08-25 17:43:47 +0100

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <I18nProvider>
         <App />
       </I18nProvider>
